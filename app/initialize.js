f682e0c9add562e7c84bb989d5a6be49abfa1009
Eric Iffland <hello@imagineiff.com>
2017-03-20 21:22:26 -0700

    Add gundb

--- app/initialize.js
+++ app/initialize.js
@@ -2,6 +2,18 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from 'components/App';
 
+var Gun = require('gun'); 
+var gun = new Gun({
+    file: 'data.json',
+    web: App,
+    s3: {
+        key: '', // AWS Access Key
+        secret: '', // AWS Secret Token
+        bucket: '' // The bucket you want to save to
+    }
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.querySelector('#app'));
 });
